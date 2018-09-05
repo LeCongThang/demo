07795e13445a57500e494b669cd7290e26194fb2
LeCongThang <hoavosac07101995@gmail.com>
2018-09-05 14:07:26 +0700

    Update navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -60,7 +60,8 @@
                             <li>
                                 <nuxt-link to="/contact">{{$t('links.contact')}}</nuxt-link>
                             </li>
-                            
+                            <li class="visible-xs-inline"><button v-on:click="changeLanguage('vi')"><img src="/images/icon/vn.png" alt=""></button>
+                                <button v-on:click="changeLanguage('en')"><img src="/images/icon/en.png" alt=""></button></li>
                         </ul>
                     </div>
                 </div>
@@ -74,96 +75,94 @@
 <script>
 import $ from "jquery";
 export default {
-  methods: {
-    changeLanguage: function(lang) {
-        this.$nuxt.$loading.start();
-      this.$store.commit("setLanguage", lang);
-      this.$i18n.locale = lang;
-      this.$nuxt.$loading.finish();
-    }
-  },
-  mounted() {
-    function fixDiv() {
-      if ($(window).width() > 992) {
-        var $cache = $("#menu");
-        var $addget = $("#fixedmobile");
-        if ($(window).scrollTop() > 42) {
-          // $('#fixed-mobile').removeClass('fix-mobile-ipad');
-          $cache.addClass("getFixed-1");
-        } else {
-          // $('#fixedmobile').addClass('fix-mobile-ipad');
-          $cache.removeClass("getFixed-1");
+    methods: {
+        changeLanguage: function (lang) {
+            this.$nuxt.$loading.start();
+            this.$store.commit("setLanguage", lang);
+            this.$i18n.locale = lang;
+            this.$nuxt.$loading.finish();
         }
-      }
-    }
+    },
+    mounted() {
+        function fixDiv() {
+            if ($(window).width() > 992) {
+                var $cache = $("#menu");
+                var $addget = $("#fixedmobile");
+                if ($(window).scrollTop() > 42) {
+                    // $('#fixed-mobile').removeClass('fix-mobile-ipad');
+                    $cache.addClass("getFixed-1");
+                } else {
+                    // $('#fixedmobile').addClass('fix-mobile-ipad');
+                    $cache.removeClass("getFixed-1");
+                }
+            }
+        }
 
-    $(window).scroll(fixDiv);
-    fixDiv();
-    $(window).resize(function() {
-      fixDiv();
-    });
-    var open = false;
-    $("#btn-toggle").on("click", function() {
-      if (open) {
-        $("#menu").removeClass("open");
-        $("#btn-toggle").removeClass("active");
-        $("body").removeClass("menu-open");
-      } else {
-        $("#menu").addClass("open");
-        $("#btn-toggle").addClass("active");
-        $("body").addClass("menu-open");
-      }
-      open = !open;
-    });
-    $("body, html").on("click", function(event) {
-      var target = $(event.target);
-      if (
-        !target.is(
-          "#menu , #menu * ,#btn-toggle ,#btn-toggle *, .header-mid, .header-mid *"
-        )
-      ) {
-        $("#menu").removeClass("open");
-        $("#btn-toggle").removeClass("active");
-        $("body").removeClass("menu-open");
-        open = false;
-      }
-    });
-    $("#menu .has-submenu .icon-first-submenu").on("click", function(event) {
-      event.preventDefault();
-      var parent = $(this).parents(".has-submenu");
-      $("#menu .has-submenu")
-        .not(parent)
-        .removeClass("open")
-        .find(".submenu-1")
-        .slideUp();
-      $("#menu .has-submenu")
-        .not(parent)
-        .find(".has-second-submenu")
-        .removeClass("open");
-      $("#menu .has-submenu")
-        .not(parent)
-        .find(".second-submenu")
-        .slideUp();
-      parent
-        .toggleClass("open")
-        .find(".submenu-1")
-        .slideToggle();
-    });
-    $("#menu .has-second-submenu .icon-second-submenu").on("click", function(
-      event
-    ) {
-      event.preventDefault();
-      var parent = $(this).parents(".has-second-submenu");
-      $("#menu .has-second-submenu")
-        .not(parent)
-        .removeClass("open")
-        .find(".submenu-2")
-        .slideUp();
-      parent
-        .toggleClass("open")
-        .find(".submenu-2")
-        .slideToggle();
-    });
-  }
+        $(window).scroll(fixDiv);
+        fixDiv();
+        $(window).resize(function () {
+            fixDiv();
+        });
+        var open = false;
+        $("#btn-toggle").on("click", function () {
+            if (open) {
+                $("#menu").removeClass("open");
+                $("#btn-toggle").removeClass("active");
+                $("body").removeClass("menu-open");
+            } else {
+                $("#menu").addClass("open");
+                $("#btn-toggle").addClass("active");
+                $("body").addClass("menu-open");
+            }
+            open = !open;
+        });
+        $("body, html").on("click", function (event) {
+            var target = $(event.target);
+            if (!target.is(
+                    "#menu , #menu * ,#btn-toggle ,#btn-toggle *, .header-mid, .header-mid *"
+                )) {
+                $("#menu").removeClass("open");
+                $("#btn-toggle").removeClass("active");
+                $("body").removeClass("menu-open");
+                open = false;
+            }
+        });
+        $("#menu .has-submenu .icon-first-submenu").on("click", function (event) {
+            event.preventDefault();
+            var parent = $(this).parents(".has-submenu");
+            $("#menu .has-submenu")
+                .not(parent)
+                .removeClass("open")
+                .find(".submenu-1")
+                .slideUp();
+            $("#menu .has-submenu")
+                .not(parent)
+                .find(".has-second-submenu")
+                .removeClass("open");
+            $("#menu .has-submenu")
+                .not(parent)
+                .find(".second-submenu")
+                .slideUp();
+            parent
+                .toggleClass("open")
+                .find(".submenu-1")
+                .slideToggle();
+        });
+        $("#menu .has-second-submenu .icon-second-submenu").on("click", function (
+            event
+        ) {
+            event.preventDefault();
+            var parent = $(this).parents(".has-second-submenu");
+            $("#menu .has-second-submenu")
+                .not(parent)
+                .removeClass("open")
+                .find(".submenu-2")
+                .slideUp();
+            parent
+                .toggleClass("open")
+                .find(".submenu-2")
+                .slideToggle();
+        });
+    }
 };
 </script>
