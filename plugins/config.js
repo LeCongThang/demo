94982caab75f029d8a8cacfb42a0dc7a4d24625b
LeCongThang <hoavosac07101995@gmail.com>
2018-09-20 21:25:47 +0700

    Update

--- plugins/config.js
+++ plugins/config.js
@@ -1,5 +1,4 @@
 export const environment ={
     domain: "http://centralreal.cf",
-    // apiUrl: "http://admin.centralreal.cf/api/v1/"
-    apiUrl: "http://centralreal.in:8/api/v1/"
+    apiUrl: "http://admin.centralreal.cf/api/v1/"
 };
